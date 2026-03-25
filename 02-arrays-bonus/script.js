ae36f0d8662c01386a27bf4bfe804194a869f046
“Nilde <nilde.benedetti@gmail.com>
2026-03-25 17:37:32 +0100

    Add solution bonus.2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -14,15 +14,32 @@ const teachers = [
 const reversedTeachers = [];
 
 for (i = (teachers.length - 1); i >= 0; i-- ) { // partiamo dall'ultimo indice dell'array; ripeti ogni volta che i e' maj o uguale a 0, ogni ciclo arretra di 1 posizione
-  currentTeacher = teachers[i]; // prendi come current teacher quello che ha i come indice
-  reversedTeachers.push(currentTeacher); // pusha il currentTeacher nell'array
+    const currentTeacher = teachers[i]; // prendi come current teacher quello che ha i come indice
+    reversedTeachers.push(currentTeacher); // pusha il currentTeacher nell'array
 }
 
  console.log(reversedTeachers); //stampami il nuovo array - ta daaaaaaaaa!!!
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = []; // creo l'array
+
+for (i = 0; i < teachers.length; i++) {  // scorri tutto l'array (for) MI RACCOMANDO SEMPRE MINORE DELLA LUNGHEZZA 
+                                          // ALTRIMENTI INCONTRA UN VALORE INDEX CHE NON ESISTE E TI FA PERDERE 80 ANNI
+                                          // PROPRIO COME OGGI
+    const currentTeacherOne = teachers[i]; // assegno variabile di appoggio per insegnante ciclo corrente
+
+    if ((teachers[i]).length >= 5) {   //SE la lunghezza del valore ciclo corrente e maj o uguale a 5 caratteri
+        longNames.push(currentTeacherOne); // aggiungo all'array
+
+    }
+}
+ console.log(longNames);
+
+
+
+  
+        //push to array
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
