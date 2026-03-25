59f6750147bdf2c61eecec875aea554d39582b0f
“Nilde <nilde.benedetti@gmail.com>
2026-03-25 18:01:02 +0100

    add soluition excercise bonus.4

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -48,7 +48,9 @@ console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.indexOf("Fabio") !== -1 ? 'yes' : 'no';
+console.log(isFabioPresent);
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
