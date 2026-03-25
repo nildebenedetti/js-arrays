e17f25b70407dbda113c51e8fe0bcea162e02ce8
“Nilde <nilde.benedetti@gmail.com>
2026-03-25 15:50:25 +0100

    add solution exerciuse 3 and 4

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -29,13 +29,11 @@ if (indexCarlo !== -1) {
 
 console.log(teachers);
 
-
-
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = null;
+const lastTeacher = teachers.pop();
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
